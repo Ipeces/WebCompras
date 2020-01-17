3afe7b54973d1aa4963938a1b9972b7257320ec1
Ipeces <59911292+Ipeces@users.noreply.github.com>
2020-01-17 14:00:26 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', '10.128.10.9');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'rootroot');
-define('DB_DATABASE', 'comprasweb');
+define('DB_DATABASE', 'sampledb');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if (!$db) {
